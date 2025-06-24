2c79f54a770cecbf0c36f3bac06f7d9c8196d88b
satishbandi201 <bandisatish20@gmail.com>
2025-06-24 17:29:51 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "roboshop" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [ aws_security_group.allow_all.id ]
   tags = {
-    Name = "HelloWorld"
+    Name = "first_server"
   }
 }
 
